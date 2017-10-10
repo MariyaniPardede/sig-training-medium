642a2b20c2259269173af73e32214691a01bbb9b
Mariyani Pardede <16053501@BTPN>
2017-10-10 15:08:11 +0700

    upload update java medium

--- src/main/java/com/netflix/simianarmy/aws/conformity/rule/BasicConformityEurekaClient.java
+++ src/main/java/com/netflix/simianarmy/aws/conformity/rule/BasicConformityEurekaClient.java
@@ -1,20 +1,3 @@
-/*
- *
- *  Copyright 2013 Netflix, Inc.
- *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
- *
- */
 package com.netflix.simianarmy.aws.conformity.rule;
 
 import com.netflix.appinfo.InstanceInfo;
@@ -26,10 +9,7 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Set;
 
-/**
- * The class implementing a client to access Eureda for getting instance information that is used
- * by Conformity Monkey.
- */
+
 public class BasicConformityEurekaClient implements ConformityEurekaClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(BasicConformityEurekaClient.class);
 
@@ -70,16 +50,13 @@ public class BasicConformityEurekaClient implements ConformityEurekaClient {
     @Override
     public boolean isHealthy(String region, String instanceId) {
         List<InstanceInfo> instanceInfos = discoveryClient.getInstancesById(instanceId);
-        if (instanceInfos.isEmpty()) {
-            LOGGER.info(String.format("Instance %s is not registered in Eureka in region %s.", instanceId, region));
+        if (instanceInfos.isEmpty()) {           
             return false;
         } else {
             for (InstanceInfo info : instanceInfos) {
                 InstanceInfo.InstanceStatus status = info.getStatus();
                 if (!status.equals(InstanceInfo.InstanceStatus.UP)
                         && !status.equals(InstanceInfo.InstanceStatus.STARTING)) {
-                    LOGGER.info(String.format("Instance %s is not healthy in Eureka with status %s.",
-                            instanceId, status.name()));
                     return false;
                 }
             }
--- src/main/java/com/netflix/simianarmy/aws/janitor/rule/volume/DeleteOnTerminationRule.java
+++ src/main/java/com/netflix/simianarmy/aws/janitor/rule/volume/DeleteOnTerminationRule.java
@@ -1,20 +1,5 @@
-/*
- *
- *  Copyright 2012 Netflix, Inc.
- *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
- *
- */
+
+
 package com.netflix.simianarmy.aws.janitor.rule.volume;
 
 import com.netflix.simianarmy.MonkeyCalendar;
@@ -31,40 +16,16 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
-/**
- * The rule is for checking whether an EBS volume is not attached to any instance and had the
- * DeleteOnTermination flag set in the previous attachment. This is an error case that AWS didn't
- * handle. The volume should have been deleted as soon as it was detached.
- *
- * NOTE: since the information came from the history, the rule will work only if Edda is enabled
- * for Janitor Monkey.
- */
+
 public class DeleteOnTerminationRule implements Rule {
 
-    /** The Constant LOGGER. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(DeleteOnTerminationRule.class);
+    
 
     private final MonkeyCalendar calendar;
 
     private final int retentionDays;
 
-    /** The date format used to print or parse the user specified termination date. **/
-    private static final DateTimeFormatter TERMINATION_DATE_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd");
-
-    /**
-     * The termination reason for the DeleteOnTerminationRule.
-     */
-    public static final String TERMINATION_REASON = "Not attached and DeleteOnTerminate flag was set";
-
-    /**
-     * Constructor.
-     *
-     * @param calendar
-     *            The calendar used to calculate the termination time
-     * @param retentionDays
-     *            The number of days that the volume is retained before being terminated after being marked
-     *            as cleanup candidate
-     */
+   
     public DeleteOnTerminationRule(MonkeyCalendar calendar, int retentionDays) {
         Validate.notNull(calendar);
         Validate.isTrue(retentionDays >= 0);
@@ -79,26 +40,25 @@ public class DeleteOnTerminationRule implements Rule {
             return true;
         }
 
-        // The state of the volume being "available" means that it is not attached to any instance.
+       
         if (!"available".equals(((AWSResource) resource).getAWSResourceState())) {
             return true;
         }
         String janitorTag = resource.getTag(JanitorMonkey.JANITOR_TAG);
         if (janitorTag != null) {
             if ("donotmark".equals(janitorTag)) {
-                LOGGER.info(String.format("The volume %s is tagged as not handled by Janitor",
-                        resource.getId()));
+               
                 return true;
             }
             try {
-                // Owners can tag the volume with a termination date in the "janitor" tag.
+                
                 Date userSpecifiedDate = new Date(
                         TERMINATION_DATE_FORMATTER.parseDateTime(janitorTag).getMillis());
                 resource.setExpectedTerminationTime(userSpecifiedDate);
                 resource.setTerminationReason(String.format("User specified termination date %s", janitorTag));
                 return false;
             } catch (Exception e) {
-                LOGGER.error(String.format("The janitor tag is not a user specified date: %s", janitorTag));
+               
             }
         }
 
@@ -107,14 +67,11 @@ public class DeleteOnTerminationRule implements Rule {
                 Date terminationTime = calendar.getBusinessDay(calendar.now().getTime(), retentionDays);
                 resource.setExpectedTerminationTime(terminationTime);
                 resource.setTerminationReason(TERMINATION_REASON);
-                LOGGER.info(String.format(
-                        "Volume %s is marked to be cleaned at %s as it is detached and DeleteOnTermination was set",
-                        resource.getId(), resource.getExpectedTerminationTime()));
-            } else {
-                LOGGER.info(String.format("Resource %s is already marked.", resource.getId()));
-            }
+                
+            } 
             return false;
         }
         return true;
     }
 }
+
